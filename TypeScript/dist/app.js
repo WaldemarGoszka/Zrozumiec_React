2066646be8359376ece56b0dbec7ab422860784d
Waldemar Goszka Athlon <goszka.waldemar@gmail.com>
2024-06-22 23:46:36 +0200

    typescript exercise1 cd

--- TypeScript/dist/app.js
+++ TypeScript/dist/app.js
@@ -1,1 +1,11 @@
 console.log("Hello2");
+let age = 18;
+// age = "eighteen"; ERROR Type 'string' is not assignable to type 'number'
+age = 20; // ok
+let ageAsString = "eighteen";
+// ageAsString = 30; ERROR Type 'number' is not assignable to type 'string'
+ageAsString = "twenty"; //ok
+ageAsString = `${age}`; //ok
+console.log(ageAsString);
+const add = (v1, v2) => v1 + v2;
+console.log(add(10, 50));
