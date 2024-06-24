75dfbdac267351d52d8c0ae03789cba6dffd35e8
WaldemarGoszka <goszka.waldemar@gmail.com>
2024-06-24 23:58:37 +0200

    Reminder forEach function

---
+++ Reminder/Table_Function/forEach.js
@@ -0,0 +1,26 @@
+const people = [
+  { name: "Jan", LastName: "Kowalski", country: "Polska", salary: 5000 },
+  { name: "Anna", LastName: "Schmidt", country: "Niemcy", salary: 2500 },
+  { name: "John", LastName: "Smith", country: "USA", salary: 8000 },
+  { name: "Maria", LastName: "Garcia", country: "Hiszpania", salary: 3500 },
+  { name: "David", LastName: "Brown", country: "UK", salary: 10000 },
+  { name: "Sophie", LastName: "Dubois", country: "Francja", salary: 6000 },
+  { name: "Freddy", LastName: "Kamionka", country: "USA", salary: 14500 },
+  { name: "Kamil", LastName: "Ślmiak", country: "Polska", salary: 4000 },
+  { name: "Luis", LastName: "Silva", country: "Portugalia", salary: 3000 },
+  { name: "Sarah", LastName: "Taylor", country: "Australia", salary: 50000 },
+];
+
+const logPersonInfo = (person, index, arr) => {
+  console.log(
+    `${index + 1} z ${arr.length}: ${person.name} ${person.LastName}, kraj: ${
+      person.country
+    }`
+  );
+};
+
+console.log("###");
+console.log(".forEach");
+console.log("###");
+
+people.forEach(logPersonInfo);
